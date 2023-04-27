a2560b2c9cedbb15452989425ce1f216791d37fc
woowooyong <woowooyong@gmail.com>
2023-04-27 14:03:10 +0800

    update: add base in config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vue-week2-Component-Modal/',
   plugins: [vue()],
   resolve: {
     alias: {
